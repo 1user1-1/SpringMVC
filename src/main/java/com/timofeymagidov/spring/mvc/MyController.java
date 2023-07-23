b90f3908158efc84c5fc549f511893b1e49916dc
1user1-1 <120933718+1user1-1@users.noreply.github.com>
2023-07-23 13:34:54 +0300

    Making required field and adding size

--- src/main/java/com/timofeymagidov/spring/mvc/MyController.java
+++ src/main/java/com/timofeymagidov/spring/mvc/MyController.java
@@ -3,11 +3,13 @@ package com.timofeymagidov.spring.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 @Controller
 
@@ -44,19 +46,15 @@ public class MyController {
 //
 //        return  "show-emp-details-view";
 //    }
-    @RequestMapping("showDetails")
-    public String showEmpDetails(@ModelAttribute("employee") Employee emp){
+    @RequestMapping( "showDetails")
+    public String showEmpDetails(@Valid @ModelAttribute("employee") Employee emp,
+                                 BindingResult bindingResult){
 
-        String name = emp.getName();
-        emp.setName("Mr. " + name);
-
-        String surname = emp.getSurname();
-        emp.setSurname(surname + "!");
-
-        int salary = emp.getSalary();
-        emp.setSalary(salary*10);
-
-        return  "show-emp-details-view";
+        if(bindingResult.hasErrors()){
+            return "ask-emp-details-view";
+        }
+        else
+            return  "show-emp-details-view";
     }
 
 }
